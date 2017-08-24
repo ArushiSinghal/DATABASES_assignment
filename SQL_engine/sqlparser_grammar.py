67fe58c2727e5f118e4aa22c94eaa86e268b30a2
Arushi Singhal <arushisinghal@gmail.com>
2017-08-24 15:39:48 +0530

    main commit

---
+++ SQL_engine/sqlparser_grammar.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+#coding=utf-8
+import numpy
+import re
+import os
+import csv
+from csv import reader
+from matplotlib import pyplot
+import sys
+import sqlparse
+import pyparsing
+from pyparsing import *
+
+selectStmt = Forward()
+SELECT = Keyword("select", caseless=True)
+FROM = Keyword("from", caseless=True)
+WHERE = Keyword("where", caseless=True)
+
+ident          = Word( alphas, alphanums + "_$" ).setName("identifier")
+columnName     = ( delimitedList( ident, ".", combine=True ) ).setName("column name")
+columnNameList = Group( delimitedList( columnName ) )
+tableName      = ( delimitedList( ident, ".", combine=True ) ).setName("table name")
+tableNameList  = Group( delimitedList( tableName ) )
+
+whereExpression = Forward()
+and_ = Keyword("and", caseless=True)
+or_ = Keyword("or", caseless=True)
+in_ = Keyword("in", caseless=True)
+
+E = CaselessLiteral("E")
+binop = oneOf("= != < > >= <= eq ne lt le gt ge", caseless=True)
+arithSign = Word("+-",exact=1)
+intNum = Combine( Optional(arithSign) + Word( nums ) + 
+            Optional( E + Optional("+") + Word(nums) ) )
+intNum = Combine( Optional(arithSign) + Word( nums ) + 
+            Optional( E + Optional("+") + Word(nums) ) )
+
+columnRval = intNum | quotedString | columnName # need to add support for alg expressions
+whereCondition = Group(
+    ( columnName + binop + columnRval ) |
+    ( columnName + in_ + "(" + delimitedList( columnRval ) + ")" ) |
+    ( columnName + in_ + "(" + selectStmt + ")" ) |
+    ( "(" + whereExpression + ")" )
+    )
+whereExpression << whereCondition + ZeroOrMore( ( and_ | or_ ) + whereExpression ) 
+
+# define the grammar
+selectStmt <<= (SELECT + ('*' | columnNameList)("columns") + 
+                FROM + tableNameList( "tables" ) + 
+                Optional(Group(WHERE + whereExpression), "")("where"))
+
+simpleSQL = selectStmt
