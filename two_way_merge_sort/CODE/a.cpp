e41a552c9017011653c4a88e725b016d1c2a4df0
Arushi Singhal <arushisinghal@gmail.com>
2017-09-10 17:29:51 +0530

    added

--- two_way_merge_sort/CODE/a.cpp
+++ two_way_merge_sort/CODE/a.cpp
@@ -12,35 +12,82 @@ using namespace std;
 int main(int argc, char* argv[]) {
   int number_of_lines = 0;
   int no_of_lines = 0;
-  string line;
-  ifstream myfile(argv[1]);
+  int column_name[3];
+  long long int sum_of_each_tuple = 0;
+  string line,p, val[4][3];
+  ifstream myfile,metafile;
   ifstream metafile;
+  myfile.open(argv[1]);
   metafile.open("metadata.txt");
-  int num = 0;
+  if (!metafile || !myfile)
+    {
+      cout << "There was an error opening the file.\n";
+      return 0;
+    }
   while (getline(myfile, line))
-  {
     ++number_of_lines;
-  }
   while (getline(metafile, line))
-  {
     ++no_of_lines;
-  }
   metafile.close();
+  myfile.close();
   metafile.open("metadata.txt");
   int num_of_rows = no_of_lines;
-  int num_of_col = 2;
-  string val[100][2];
   for(int countRows = 0; countRows < num_of_rows; countRows++)
     {
-        for(int countColumns = 0; countColumns < num_of_col; countColumns++)
+        for(int countColumns = 0; countColumns < 2; countColumns++)
         {
             metafile >> val[countRows][countColumns];
-            cout << val[countRows][countColumns] << '\n';
+            if (countColumns > 0)
+            sum_of_each_tuple += stoi(val[countRows][countColumns]);
         }
     }
   metafile.close();
   int memory = stoi(argv[3]);
   int cal = argc - 5;
-  cout << cal << '\n';
+  for (int i = 0;i<cal;i++)
+  {
+    p = argv[i+5];
+    for (int k=0;k<num_of_rows;k++)
+    {
+      if (p == val[k][0])
+      {
+      column_name[i] = k;
+      break;
+      }
+    }
+  }
+  string a1 = "asc";
+  string a2 = "desc";
+  if (argv[4] == a1)
+  {
+      //column_name, sum_of_each_tuple, cal, a1, memory, number_of_lines,iteration,num_of_rocord(block);
+      long long int num_of_record = memory/sum_of_each_tuple;
+      long long int iteration = number_of_lines/num_of_record;
+      if (number_of_lines%num_of_record != 0)
+        iteration += 1;
+      for (long long int l = 1; l<= iteration; l++)
+      {
+        /*
+         for(int countRows = 0; countRows < num_of_rows; countRows++)
+          {
+              for(int countColumns = 0; countColumns < num_of_col; countColumns++)
+              {
+                  metafile >> val[countRows][countColumns];
+                  if (countColumns > 0)
+                  sum_of_each_tuple += stoi(val[countRows][countColumns]);
+              }
+          }
+        */
+      }
+  }
+  else if (argv[4] == a2)
+  {
+    column_name, sum_of_each_tuple, cal, a2, memory, number_of_lines;
+  }
+  else
+  {
+    cout << "type correct order in which want to sort the file.\n";
+    return 0;
+  }
   return 0;
 }
