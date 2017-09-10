76da77e3eb7395c34515b0e2f0227d9dffb107b2
Arushi Singhal <arushisinghal@gmail.com>
2017-09-10 10:05:56 +0530

    changes

---
+++ two_way_merge_sort/CODE/201516178.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install
+sudo apt-get install vim
+g++ -std=c++11 -static a.cpp
+./a.out input.txt output.txt 100
--- two_way_merge_sort/CODE/a.cpp
+++ two_way_merge_sort/CODE/a.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <iterator>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+#include <stdexcept>
 using namespace std;
+int main(int argc, char* argv[]) {
+  int number_of_lines = 0;
+  string line;
+  ifstream myfile(argv[1]);
+  while (getline(myfile, line))
+  {
+    ++number_of_lines;
+  }
+  int memory = stoi(argv[3]);
+  int cal = argc - 5;
+  cout << cal << '\n'; 
+  return 0;
+}
