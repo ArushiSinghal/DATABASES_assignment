9f8d6258b62918844549769c35ece76249a03302
Arushi Singhal <arushisinghal@gmail.com>
2017-09-18 18:16:06 +0530

    submission

--- two_way_merge_sort/CODE/a.cpp
+++ two_way_merge_sort/CODE/a.cpp
@@ -14,8 +14,10 @@
 #include <cstring>
 #include <ios>
 using namespace std;
+string a[6000000];
+long long int b[6000000];
 
-int arrange(string split1[100],string split2[100],long long int b[10000],long long int cal,long long int column_name[1000],string a[10000] ,long long int i, long long int j, string a1)
+int arrange(string split1[100],string split2[100],long long int cal,long long int column_name[1000],long long int i, long long int j, string a1)
 {
   string aa = "asc";
   string bb = "desc";
@@ -64,7 +66,37 @@ int arrange(string split1[100],string split2[100],long long int b[10000],long lo
   return 0;
 }
 
-void sort(string a[10000],long long int column_name[1000],long long int cal,string a1,long long int num_of_record,long long int b[10000],string val[20][3],long long int num_rows)
+
+void sort_phase(long long int column_name[1000],long long int cal,string a1,long long int num_of_record,string val[20][3],long long int num_rows)
+{
+  string split1[100],split2[100];
+  char spaceman = ' ';
+  long long int num2;
+    int count11 = 0;
+    for (long long int f = 0;f<num_rows;f++)
+    {
+      split1[f] = a[0].substr(2*f+count11,stoi(val[f][1]));
+      count11 += stoi(val[f][1]);
+    }
+    for (long long int j=1;j<num_of_record;j++) {
+      count11 = 0;
+      for (long long int f = 0;f<num_rows;f++)
+      {
+        split2[f] = a[j].substr(2*f+count11,stoi(val[f][1]));
+        count11 += stoi(val[f][1]);
+      }
+      num2 = num_rows;
+        int flag = arrange(split1,split2,cal,column_name,0,j,a1);
+      if (flag == 1)
+      {
+        for (int m=0;m<num2;m++)
+          split1[m] = split2[m];
+      }
+      }
+  return;
+}
+
+void sort(long long int column_name[1000],long long int cal,string a1,long long int num_of_record,string val[20][3],long long int num_rows)
 {
   string split1[100],split2[100];
   char spaceman = ' ';
@@ -85,7 +117,7 @@ void sort(string a[10000],long long int column_name[1000],long long int cal,stri
         count11 += stoi(val[f][1]);
       }
       num2 = num_rows;
-      int flag = arrange(split1,split2,b,cal,column_name,a,i,j,a1);
+	int flag = arrange(split1,split2,cal,column_name,i,j,a1);
       if (flag == 1)
       {
         for (int m=0;m<num2;m++)
@@ -95,40 +127,14 @@ void sort(string a[10000],long long int column_name[1000],long long int cal,stri
     }
   return;
 }
-/*
-void sort(string a[10000],long long int column_name[1000],long long int cal,string a1,long long int num_of_record,long long int b[10000])
-{
-  string split1[100],split2[100];
-  char spaceman = ' ';
-  long long int num1,num2;
-  for (long long int i=0;i<num_of_record;i++) {
-      split1[0] = a[i].substr(0,10);
-	      split1[1] = a[i].substr(12,32);
-	       split1[2] = a[i].substr(46,52);
-         num1 = 3;
-    for (long long int j=i+1;j<num_of_record;j++) {
-          split2[0] = a[j].substr(0,10);
-   	      split2[1] = a[j].substr(12,32);
-   	       split2[2] = a[j].substr(46,52);
-          num2 = 3;
-      int flag = arrange(split1,split2,b,cal,column_name,a,i,j,a1);
-      if (flag == 1)
-      {
-        for (int m=0;m<num2;m++)
-          split1[m] = split2[m];
-      }
-      }
-    }
-  return;
-}
-*/
+
 int main(int argc, char* argv[]) {
   long long int number_of_lines = 0;
   long long int no_of_lines = 0;
-  long long int column_name[1000],b[10000],num_of_rows,iteration;
+  long long int column_name[1000],num_of_rows,iteration;
   long long int sum_of_each_tuple = 0;
-  string line, p, val[4][3], a[10000];
-  long long int memory = stoi(argv[3]);
+  string line, p, val[4][3];
+  long long int memory = stoi(argv[3])*1048576;
   long long int cal = argc - 5;
   long long int var = 0;
   long long int new_num_record;
@@ -158,7 +164,7 @@ int main(int argc, char* argv[]) {
             sum_of_each_tuple += stoi(val[countRows][countColumns]);
         }
     }
-  sum_of_each_tuple = sum_of_each_tuple + 2*(num_of_rows-1);
+  sum_of_each_tuple = sum_of_each_tuple + 2*(num_of_rows-1) + 2;
   metafile.close();
 
   for (int i = 0;i<cal;i++)
@@ -181,8 +187,8 @@ int main(int argc, char* argv[]) {
   }
 
   ////////////////////////PHASE 1//////////////////////////////////////////////
-  ofstream ofs("output1.txt", ios::out | ios::trunc);
-  ofs.close ();
+  //ofstream ofs("output1.txt", ios::out | ios::trunc);
+  //ofs.close ();
   string a1 = "asc";
   string a2 = "desc";
   myfile.open(argv[1]);
@@ -203,13 +209,17 @@ int main(int argc, char* argv[]) {
             getline(myfile, a[k], '\n');
             b[k] = k;
         }
-        sort (a,column_name,cal,argv[4],num_of_record,b,val,no_of_lines);
-        for (long long int i= 0;i<num_of_record;i++)
-        {
-          ofstream log("output1.txt", std::ios_base::app | std::ios_base::out);
-          a[i] = a[i].substr(0, a[i].size());
-          log << a[i] << "\n";
-        }
+	sort (column_name,cal,argv[4],num_of_record,val,no_of_lines);
+          string filenames = "firstsort";
+          filenames = filenames + to_string(l);
+          filenames =  filenames + ".txt";
+          ofstream ofs(filenames, ios::out | ios::trunc);
+          for (long long int i= 0;i<num_of_record;i++)
+          {
+            ofstream log(filenames, std::ios_base::app | std::ios_base::out);
+            a[i] = a[i].substr(0, a[i].size());
+            log << a[i] << "\n";
+          }
       }
   }
   else
@@ -218,6 +228,43 @@ int main(int argc, char* argv[]) {
     return 0;
   }
 	myfile.close();
-/////SECOND PHASE OF THE SORT
+////////////////////////////////second phase///////////////////////
+ifstream myfiles[2000];
+for(int i=1;i<=iteration;i++)
+{
+  string file = "firstsort" + to_string(i) + ".txt";
+	myfiles[i].open(file);
+}
+int count;
+ofstream ofs(argv[2], ios::out | ios::trunc);
+while(1)
+{
+count = 0;
+  for(int i=1;i<=iteration;i++)
+  {
+    if(myfiles[i].eof() || myfiles[i].peek() == EOF)
+    {
+      count +=1;
+      count = count -1;
+    }
+    else
+    {
+      streampos oldpos = myfiles[i].tellg();  // stores the position
+      getline(myfiles[i], a[count], '\n');
+      myfiles[i].seekg (oldpos);   // get back to the position
+      b[count] = i;
+      count +=1;
+    }
+  }
+
+  if (count == 0)
+  break;
+  sort_phase(column_name,cal,argv[4],count,val,no_of_lines);
+  //cout << a[0] << "\n";
+  ofstream log(argv[2], ios_base::app | ios_base::out);
+  a[0] = a[0].substr(0, a[0].size());
+  log << a[0] << "\n";
+  getline(myfiles[b[0]], a[0], '\n');
+}
 	return 0;
 }
