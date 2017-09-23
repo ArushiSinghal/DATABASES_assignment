a56fa5d1954996f807db0f6692ffa4ce902278bf
Arushi Singhal <arushisinghal@gmail.com>
2017-09-23 19:18:58 +0530

    changes

--- two_way_merge_sort/CODE/201516178.sh
+++ two_way_merge_sort/CODE/201516178.sh
@@ -5,5 +5,8 @@ sudo apt-get install
 sudo apt-get install vim
 ./gensort -a 100 input.txt
 ./valsort output.txt
-g++ -std=c++11 -static a.cpp
+#first-phase with bubble sort
+g++ -std=c++11 -static av1.cpp
+#first-phase with merge sort
+g++ -std=c++11 -static av2.cpp
 ./a.out input.txt output.txt 100 desc c0 c1 c2
