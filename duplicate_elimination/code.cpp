8f58be71efa79b22f9a33736c3962c14cd8d1864
Arushi Singhal <arushisinghal@gmail.com>
2017-09-25 19:37:08 +0530

    changes

--- duplicate_elimination/code.cpp
+++ duplicate_elimination/code.cpp
@@ -15,22 +15,54 @@
 #include <ios>
 #include <ctime>
 using namespace std;
+int input_file_generate(int attributes,int  duplicate)
+{
+	int j,i;
+	string A[100];
+	string filenames =  "input.csv";
+	ofstream ofs(filenames, ios::out | ios::trunc);
+	while(1)
+	{
+		ifstream in(filenames, ifstream::ate | ifstream::binary);
+		long long int memory = in.tellg();
+		if(memory > 1073741824)
+			break;
+		ofstream log(filenames, std::ios_base::app | std::ios_base::out);
+		for(i=0;i<(100-duplicate);i++)
+		{
+			int a;
+			if(attributes == 1)
+			{
+				a = rand()%1001;
+				log << a << "\n";
+				A[i] = to_string(a);
+			}
+			else
+			{
+				a = rand()%1001;
+				log << a << ",";
+				A[i] = to_string(a) + ",";
+				for(j=2;j<=attributes-1;j++)
+				{
+					a = rand()%1001;
+					log << a << ",";
+					A[i] = A[i] + to_string(a) + ",";
+				}
+				a = rand()%1001;
+				log << a << "\n";
+				A[i] = A[i] + to_string(a);
+			}
+		}
+		for(i=0;i<duplicate;i++)
+		{
+			int val = rand()%(100-duplicate);
+			log << A[val] << "\n";
+		}
+	}
+	return 0;
+}
 int main(int argc, char* argv[])
 {
-	//numberofatributes argv[1]
-	//numberofrows arg[2]
-	//percentage ofduplicacy argv[3]
-	long long int attributes = stoi(argv[1]);
-	long long int rows = stoi(argv[2]);
-	long long int duplicate = (rows*(stoi(argv[3])))/100;
-	long long int no_duplicate = rows - duplicate;
-	for (int i=0;i<100;i++)
-	{
-		
-		int a = rand()%1001;
-		if (a==0)
-			cout << "1" << "\n";
-		cout << a  << "\n";
-	}
+	input_file_generate(stoi(argv[1]),stoi(argv[2]));
 	return 0;
 }
