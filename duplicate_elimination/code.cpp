c564129af6550138d4c236e25f1135ebaf6b0ccf
Arushi Singhal <arushisinghal@gmail.com>
2017-09-27 22:05:08 +0530

    changes

--- duplicate_elimination/code.cpp
+++ duplicate_elimination/code.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -15,12 +16,18 @@
 #include <ios>
 #include <ctime>
 using namespace std;
-int input_file_generate(int attributes,int  duplicate)
+long long int distinct_tuples, tuples_one_block, total_tuples, number_of_blocks;
+string filenames =  "input.csv";
+string output_file = "output.csv";
+
+/**************************** INPUT FILE GENERATION ********************************/
+int input_file_generate(int attributes,int  duplicate, long long int block)
 {
 	int j,i;
 	string A[100];
-	string filenames =  "input.csv";
+	filenames =  "input.csv";
 	ofstream ofs(filenames, ios::out | ios::trunc);
+	long long int count = 0;
 	while(1)
 	{
 		ifstream in(filenames, ifstream::ate | ifstream::binary);
@@ -28,6 +35,7 @@ int input_file_generate(int attributes,int  duplicate)
 		if(memory > 1073741824)
 			break;
 		ofstream log(filenames, std::ios_base::app | std::ios_base::out);
+		count += 1;
 		for(i=0;i<(100-duplicate);i++)
 		{
 			int a;
@@ -59,10 +67,110 @@ int input_file_generate(int attributes,int  duplicate)
 			log << A[val] << "\n";
 		}
 	}
+	distinct_tuples = count*100- duplicate*count;
+	tuples_one_block = distinct_tuples/block;
+	total_tuples = count *100;
+	ofs.close();
 	return 0;
 }
+
+/**************  duplicacy elimination using two phase hashing **********************************************/
+int hashing(long long int attributes)
+{
+	ifstream input_file(filenames);
+	long long int i,j;
+	string data;
+	ifstream fi;
+	fi.open(filenames);
+	string b,a;
+	ifstream myfiles[2000];
+	for(int i=0;i<number_of_blocks;i++)
+	{
+		string file = "firstsort" + to_string(i) + ".csv";
+		myfiles[i].open(file);
+		ofstream files(file, ios::out | ios::trunc);
+
+	}
+	for(i=0;i<total_tuples;i++)
+	{
+		getline(fi, a, '\n');
+		long long int count = 0;
+		if (i==total_tuples-1)
+		{
+			for(j=0;j<attributes-1;j++)
+			{
+				getline(input_file, data, ',');
+				count  += stoi(data);
+			}
+			getline(input_file, data);
+			count += stoi(data);
+		}
+		else
+		{
+			for(j=0;j<attributes-1;j++)
+			{
+				getline(input_file, data, ',');
+				count += stoi(data);
+			}
+		}
+		count = count%(number_of_blocks);
+		string file = "firstsort" + to_string(count) + ".csv";
+		ofstream log(file, ios_base::app | ios_base::out);
+		log << a << "\n";
+	}
+	/*
+	for(int i=0;i<number_of_blocks;i++)
+	{
+		file = "secondsort" + to_string(i) + ".csv";
+		myfiles[i].open(file);
+		ofstream files(file, ios::out | ios::trunc);
+  }
+	for(int i=0;i<number_of_blocks;i++)
+	{
+		string file = "firstsort" + to_string(i) + ".csv";
+		ifstream file_check(file);
+		string p,c;
+		while(!file_check.eof())
+		{
+			getline(file_check, p, '\n');
+			file = "secondsort" + to_string(i) + ".csv";
+			ifstrem no_duplicate(secondsort)
+			int  k =0;
+			while(!no_duplicate.eof())
+			{
+			getline(no_duplicate, c, '\n');
+			if (p==c)
+			{
+			k = 1;
+			break;
+		}
+		}
+		if (k==0)
+		{
+		string file = "secondsort" + to_string(i) + ".csv";
+		ofstream log(file, ios_base::app | ios_base::out);
+		log << p << "\n";
+	}
+	 }
+	}
+  */
+	return 0;
+}
+int btree()
+{
+	return 0;
+}
+
 int main(int argc, char* argv[])
 {
-	input_file_generate(stoi(argv[1]),stoi(argv[2]));
+	//Attribute, duplicacy percentage, number of blocks in main memory, type_of_index
+	input_file_generate(stoi(argv[1]),stoi(argv[2]), stoi(argv[3]));
+	number_of_blocks = stoi(argv[3]);
+	string index_type =  argv[4];
+	string a = "hash";
+	if (a == index_type)
+		hashing(stoi(argv[1]));
+	else
+		btree();
 	return 0;
 }
