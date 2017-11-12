a67a602f7b6ba7a3d3057a49f0625d7a209006d2
Arushi Singhal <arushisinghal@gmail.com>
2017-11-12 14:13:03 +0530

    changes

--- Implementing_Join/201516178_Assign4/201516178.sh
+++ Implementing_Join/201516178_Assign4/201516178.sh
@@ -4,4 +4,5 @@ sudo apt-get upgrade
 sudo apt-get install
 sudo apt-get install vim
 g++ -std=c++11 -static code.cpp
+time ./a.out /home/seema/DBMS/Implementing_Join/201516178_Assign4/input/R /home/seema/DBMS/Implementing_Join/201516178_Assign4/input/S sort 100
 time ./a.out /home/seema/DBMS/Implementing_Join/201516178_Assign4/input/R /home/seema/DBMS/Implementing_Join/201516178_Assign4/input/S hash 100
