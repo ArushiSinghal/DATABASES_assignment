22642f9020f3546612c8888986a6ad47c8fe9f92
Arushi Singhal <arushisinghal@gmail.com>
2017-11-22 02:24:00 +0530

    changes

--- Implementing_Join/201516178_Assign4/code/code.cpp
+++ Implementing_Join/201516178_Assign4/code/code.cpp
@@ -21,6 +21,9 @@ long long int b[6000000];
 string leff[6000000];
 long long int b1[6000000];
 int flag_sort;
+long long int number_of_blocks;
+ifstream input_file;
+
 int Merge_second(long long int ini,long long int mid,long long int fin)
 {
 	for(long long int i=ini;i<=fin;i++)
@@ -216,18 +219,142 @@ int main_function(string input_filesname,string output_filenames ,long long int 
 	return 0;
 }
 
+void first_phase_r(string in_file)
+{
+	long long int i,j;
+	string a,data;
+	for(int i=0;i<number_of_blocks;i++)
+	{
+		string file = "firstsort" + to_string(i);
+		ofstream files(file, ios::out | ios::trunc);
+		files.close();
+	}
+	ifstream myfile;
+	myfile.open(in_file);
+	string line;
+	while(getline(myfile, line))
+	{
+		long long int aa, bb;
+		istringstream twonumbers;
+		twonumbers.str(line);
+		twonumbers >> aa >> bb;
+		long long int data = bb%number_of_blocks;
+		string file = "firstsort" + to_string(data);
+		ofstream log(file, ios_base::app | ios_base::out);
+		log << line << "\n";
+		log.close();
+	}
+	return;
+}
+void first_phase_s(string in_file)
+{
+        long long int i,j;
+        string a,data;
+        for(int i=0;i<number_of_blocks;i++)
+        {
+                string file = "secondsort" + to_string(i);
+                ofstream files(file, ios::out | ios::trunc);
+                files.close();
+        }
+				ifstream myfile;
+				myfile.open(in_file);
+				string line;
+        while(getline(myfile, line))
+        {
+
+                long long int aa, bb;
+                istringstream twonumbers;
+                twonumbers.str(line);
+                twonumbers >> aa >> bb;
+                long long int data = aa%number_of_blocks;
+                string file = "secondsort" + to_string(data);
+                ofstream log(file, ios_base::app | ios_base::out);
+                log << line << "\n";
+								log.close();
+        }
+        return;
+}
+
+int hash_join(string input_filename1, string input_filename2, long long int num_blocks)
+{
+	string filenames =  input_filename1;
+	number_of_blocks = num_blocks;
+	input_file.open(filenames);
+	first_phase_r(input_filename1);
+	input_file.close();
+	filenames =  input_filename2;
+	input_file.open(filenames);
+	first_phase_s(input_filename2);
+	input_file.close();
+}
+
+int main_hashing(string input_filename1, string input_filename2,string output_file, long long int num_blocks)
+{
+	hash_join(input_filename1, input_filename2, num_blocks);
+	ofstream v(output_file, ios::out | ios::trunc);
+	v.close();
+	ofstream ofs(output_file, ios_base::app | ios_base::out);
+	for(int i=0;i<number_of_blocks;i++)
+	{
+		ifstream myfile, myfile1;
+		string name1 = "firstsort" + to_string(i);
+		string name2 = "secondsort" + to_string(i);
+		myfile.open(name1);
+		myfile1.open(name2);
+		while(1)
+			{
+				string line1, line2, out;
+				if(myfile.eof() || myfile.peek() == EOF || myfile1.eof() || myfile1.peek() == EOF)
+					break;
+				getline(myfile, line1);
+				streampos oldpos1 = myfile1.tellg();  // stores the position
+				getline(myfile1, line2);
+			  int aa, bb, cc, dd;
+				string aaa, bbb, ccc, ddd, var1, var2;
+				istringstream twonumbers,twonumbers1, stringtwo1, stringtwo2;
+				stringtwo1.str(line1);
+				stringtwo2.str(line2);
+			  stringtwo1 >> aaa >> bbb;
+				stringtwo2 >> ccc >> ddd;
+				twonumbers.str(line1);
+				twonumbers >> aa >> bb;
+				var1 = bb;
+				twonumbers1.str(line2);
+				twonumbers1 >> cc >> dd;
+				var2 = cc;
+				if (var1 == var2)
+				{
+					out = aaa + " " + bbb + " " + ddd;
+		  		ofs << out << "\n";
+				}
+				while(getline(myfile1, line2))
+				{
+						istringstream twonumbers3, stringtwo3;
+						twonumbers3.str(line2);
+					  twonumbers3 >> cc >> dd;
+						stringtwo3.str(line2);
+						stringtwo3 >> ccc >> ddd;
+						var2 = cc;
+						if (var2 == var1)
+						{
+							out = aaa + " " + bbb + " " + ddd;
+							ofs << out << "\n";
+						}
+				}
+				myfile1.close();
+				myfile1.open(name2);
+				myfile1.seekg(oldpos1);
+			}
+	}
+	ofs.close();
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
 	string r_filename_path =  argv[1];
 	string s_filename_path =  argv[2];
 	string type = argv[3];
-	string output1_sort = "output1";
-	string output2_sort = "output2";
-	ofstream v1(output1_sort, ios::out | ios::trunc);
-        v1.close();
-	ofstream v2(output1_sort, ios::out | ios::trunc);
-        v2.close();
 	long long int buff  = stoi(argv[4]);
 	long long int var1 = r_filename_path.length();
 	long long int var2 = s_filename_path.length();
@@ -251,10 +378,19 @@ int main(int argc, char* argv[])
 	string output_file = r_filename + "_" + s_filename + "_" + "join";
 	ofstream v(output_file, ios::out | ios::trunc);
 	v.close();
+
+if (type == "sort")
+{
+	string output1_sort = "output1";
+	string output2_sort = "output2";
+	ofstream v1(output1_sort, ios::out | ios::trunc);
+        v1.close();
+	ofstream v2(output1_sort, ios::out | ios::trunc);
+        v2.close();
 	flag_sort = 0;
 	main_function(r_filename_path, output1_sort,buff);
 	flag_sort = 1;
-        main_function(s_filename_path, output2_sort,buff);
+  main_function(s_filename_path, output2_sort,buff);
 	ifstream myfile,myfile1;
 	myfile.open(output1_sort);
 	myfile1.open(output2_sort);
@@ -262,14 +398,10 @@ int main(int argc, char* argv[])
 	while(1)
 	{
 		string line1, line2, out;
-		if(myfile.eof() || myfile.peek() == EOF)
+		if(myfile.eof() || myfile.peek() == EOF || myfile1.eof() || myfile1.peek() == EOF)
     			{
 				break;
 			}
-		if(myfile1.eof() || myfile1.peek() == EOF)
-                        {
-                                break;
-                        }
 		streampos oldpos = myfile.tellg();  // stores the position
       		getline(myfile, line1);
       		//myfile.seekg (oldpos);   // get back to the position
@@ -332,5 +464,11 @@ int main(int argc, char* argv[])
 	ofs.close();
 	myfile.close();
 	myfile1.close();
+}
+else
+{
+ main_hashing(r_filename_path, s_filename_path, output_file, buff);
+}
+
 	return 0;
 }
